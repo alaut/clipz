11c0adbbb5c38e00230ac6c97b78a425f1c2d7ae
Alexander Laut <14655890+alaut@users.noreply.github.com>
2020-07-12 23:15:24 -0700

    cleaned up and functional

--- clipz.py
+++ clipz.py
@@ -1,39 +1,30 @@
-import csv
-import os
-from datetime import datetime
-from subprocess import call
-from pathlib import Path
-
-# import datetime
 import yaml
+from datetime import datetime
+import os
 
-
-def clipz(input_file, check=True):
+def clipz(input_file, test=True, output_folder = '.\\'):
     print('Loading {}...'.format(input_file))
     with open(input_file) as f:
         clips = yaml.load(f, Loader=yaml.FullLoader)
 
     for clip in clips:
         input_video = list(clip.keys())[0]
-        # print(input_video)
+
+        try:
+            os.mkdir(output_folder)
+        except:
+            pass
 
         for start, stop, output_video in clip[input_video]:
-            # print('{}-{}\t{}'.format(start, stop, output_video))
-            fmt = '%M:%S'
-            delta = datetime.strptime(stop, fmt) - datetime.strptime(start, fmt)
-
+            print('{}-{}\t{}'.format(start, stop, output_video))
+  
+            delta = datetime.strptime(stop, '%M:%S') - datetime.strptime(start, '%M:%S')
             input_path = os.path.abspath(input_video)
-            output_path = os.path.abspath(output_video)
+            output_path = os.path.abspath(output_folder +'\\' + output_video)
 
             cmd = 'ffmpeg -i "{:}" -ss {:} -t {:} "{:}"'.format(input_path, start, str(delta), output_path)
-
-
-            # cmd = 'ffmpeg -i "'+fin+'" -ss '+t0+' -t '+dt+' "'+fout+'"'
-            print(cmd)
-            if not(check):
-                # os.system(cmd)
-                pass
-
-
-if __name__=="__main__":
-    clipz('.\example.yaml')
+            
+            if test:
+                print(cmd)
+            else:
+                os.system(cmd)
